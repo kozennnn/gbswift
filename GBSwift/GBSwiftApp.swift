6dec7f146baf9afef86e64a19f5e8ec01039053f
Mathis HOUIS <mathishouis@gmail.com>
2022-12-12 18:45:42 +0100

    Initial Commit

---
+++ GBSwift/GBSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBSwiftApp.swift
+//  GBSwift
+//
+//  Created by Mathis HOUIS on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GBSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
